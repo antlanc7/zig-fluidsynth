bba130d84ba9bd18825f50ff034702c370ed2691
Antonio Lanciotti <a.lanciotti@westernengineering.it>
2025-07-21 14:35:44 +0200

    cleanup build.zig, add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Zig-Fluidsynth
+
+A CLI Frontend for libfluidsynth
+
+Works on terminal but it also expose a TCP server.
+
+The project depends on system installed `fluidsynth`, and so it's working only on Unix (tested on MacOS).  
+On Windows I couldn't figure out a standard way to link fluidsynth.
+
+> TODO: Investigate a way to compile `fluidsynth` with `build.zig` to use it as a dependency with `zig fetch` without dynamic linking (this would work also for Windows).
+
--- build.zig
+++ build.zig
@@ -4,42 +4,24 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // We will also create a module for our other entry point, 'main.zig'.
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     const exe = b.addExecutable(.{
         .name = "zig_fluidsynth",
-        .root_module = exe_mod,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
     });
 
     exe.linkSystemLibrary("fluidsynth");
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
