ec224209eb577dc38ec3bbb923ff5ada3b93535f
Antonio Lanciotti <a.lanciotti@westernengineering.it>
2025-10-31 14:45:10 +0100

    update to fluidsynth 2.5.1 for windows

--- build.zig
+++ build.zig
@@ -21,16 +21,11 @@ pub fn build(b: *std.Build) void {
             exe_mod.addLibraryPath(fluidsynth.path("lib"));
             exe_mod.linkSystemLibrary("libfluidsynth-3", .{});
             const fluidsynth_dll = b.addInstallBinFile(fluidsynth.path("bin/libfluidsynth-3.dll"), "libfluidsynth-3.dll");
+            const sndfile_dll = b.addInstallBinFile(fluidsynth.path("bin/sndfile.dll"), "sndfile.dll");
+            const sdl3_dll = b.addInstallBinFile(fluidsynth.path("bin/SDL3.dll"), "SDL3.dll");
+            b.getInstallStep().dependOn(&sndfile_dll.step);
+            b.getInstallStep().dependOn(&sdl3_dll.step);
             b.getInstallStep().dependOn(&fluidsynth_dll.step);
-
-            // FIXME: when https://github.com/FluidSynth/fluidsynth/pull/1689 is merged and a new version is released this dependency can be removed
-            // basically in the 2.5.0 release they forgot to include the `sndfile.dll` and `SDL3.dll` so we take them from the 2.4.8 release
-            if (b.lazyDependency("fluidsynth_dlls", .{})) |fluidsynth_dlls| {
-                const sndfile_dll = b.addInstallBinFile(fluidsynth_dlls.path("bin/sndfile.dll"), "sndfile.dll");
-                const sdl3_dll = b.addInstallBinFile(fluidsynth_dlls.path("bin/SDL3.dll"), "SDL3.dll");
-                b.getInstallStep().dependOn(&sndfile_dll.step);
-                b.getInstallStep().dependOn(&sdl3_dll.step);
-            }
         }
     } else {
         exe_mod.linkSystemLibrary("fluidsynth", .{});
